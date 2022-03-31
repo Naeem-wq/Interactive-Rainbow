94822643a3eeeabf628961e69e1b9c89ad9f5a11
Naeem-wq <95334435+Naeem-wq@users.noreply.github.com>
2022-03-31 12:10:52 +0200

    Added buttons, styling and JavaScript.
    Created an Interactive rainbow where the user clicks on 7 buttons stacked on each other, with each button displaying the colors of the rainbow from top to bottom once clicked on.

---
+++ assets/styles.js
@@ -0,0 +1,56 @@
+function func1(){
+    document.getElementById("btn1").style.backgroundColor = "red";
+    document.getElementById("btn1").innerHTML = "RED";
+}
+
+function func2(){
+    document.getElementById("btn2").style.backgroundColor = "orange";
+    document.getElementById("btn2").innerHTML = "ORANGE";
+}
+
+function func3(){
+    document.getElementById("btn3").style.backgroundColor = "yellow";
+    document.getElementById("btn3").innerHTML = "YELLOW";
+}
+
+function func4(){
+    document.getElementById("btn4").style.backgroundColor = "green";
+    document.getElementById("btn4").innerHTML = "GREEN";
+}
+
+function func5(){
+    document.getElementById("btn5").style.backgroundColor = "blue";
+    document.getElementById("btn5").innerHTML = "BLUE";
+}
+
+function func6(){
+    document.getElementById("btn6").style.backgroundColor = "indigo";
+    document.getElementById("btn6").innerHTML = "INDIGO";
+}
+
+function func7(){
+    document.getElementById("btn7").style.backgroundColor = "violet";
+    document.getElementById("btn7").innerHTML = "VIOLET";
+}
+
+window.addEventListener("load", function() {
+    var elements = document.getElementsByClassName("rainbowText");
+    for (let i = 0; i < elements.length; i++) {
+      generateRainbowText(elements[i]);
+    }
+  });
+  
+
+//                  Rainbow Heading. 
+//   (GOT THIS ON STACK OVERFLOW AND REALLY LIKED IT.)
+  
+  function generateRainbowText(element) {
+    var text = element.innerText;
+    element.innerHTML = "";
+    for (let i = 0; i < text.length; i++) {
+      let charElem = document.createElement("span");
+      charElem.style.color = "hsl(" + (360 * i / text.length) + ",80%,50%)";
+      charElem.innerHTML = text[i];
+      element.appendChild(charElem);
+    }
+  } 
